0cd8832b0085129a3a568379134796ec28d1e961
Valerii Kravets <viniokil@gmail.com>
2020-09-19 04:01:48 +0000

    (update) code refactored

--- volumes-provisioner.go
+++ volumes-provisioner.go
@@ -15,47 +15,61 @@ type dirProperties struct {
 	path string
 }
 
+func (d dirProperties) chmod() {
+	if err := os.Chmod(d.path, d.mode); err != nil {
+		log.Error(err)
+	} else {
+		log.Info("Chmod dir ", d.path, " to mode ", d.mode)
+	}
+}
+
+func (d dirProperties) chown() {
+	if err := os.Chown(d.path, d.uid, d.gid); err != nil {
+		log.Error(err)
+	} else {
+		log.Info("Chown dir ", d.path, " to uid ", d.uid, " and gid ", d.gid)
+	}
+}
+
 func parseConfig() []dirProperties {
 	var dirsConfig []dirProperties
 
 	envVarVvalue, envVarExist := os.LookupEnv("PROVISION_DIRECTORIES")
-	if envVarExist {
-		var provisionDirConfigs []string = strings.Split(envVarVvalue, ";")
-		for _, dir := range provisionDirConfigs {
-			var properties []string = strings.Split(dir, ":")
+	if !envVarExist {
+		return nil
+	}
 
-			log.Debug("Dir properties:", properties)
+	var provisionDirConfigs []string = strings.Split(envVarVvalue, ";")
 
-			uid, _ := strconv.Atoi(properties[0])
-			gid, _ := strconv.Atoi(properties[1])
-			modeUint, _ := strconv.ParseUint(properties[2], 0, 32)
-			mode := os.FileMode(modeUint)
-			path := properties[3]
+	for _, dir := range provisionDirConfigs {
+		var properties []string = strings.Split(dir, ":")
 
-			log.Debug("Parsed values", "uid: ", uid, ", gid: ", gid, ", mode: ", mode, ", path: ", path)
+		log.Debug("Dir properties:", properties)
 
-			var config = dirProperties{uid, gid, mode, path}
+		uid, _ := strconv.Atoi(properties[0])
+		gid, _ := strconv.Atoi(properties[1])
+		modeUint, _ := strconv.ParseUint(properties[2], 8, 32)
+		mode := os.FileMode(modeUint)
+		path := properties[3]
 
-			dirsConfig = append(dirsConfig, config)
+		log.Debug("Parsed values", "uid: ", uid, ", gid: ", gid, ", mode: ", mode, ", path: ", path)
 
-		}
+		var config = dirProperties{uid, gid, mode, path}
+
+		dirsConfig = append(dirsConfig, config)
 	}
 
 	return dirsConfig
 }
 
 func main() {
-	for _, dir := range parseConfig() {
-		if err := os.Chmod(dir.path, dir.mode); err != nil {
-			log.Error(err)
-		} else {
-			log.Info("Chmod dir ", dir.path, " to mode ", dir.mode)
-		}
 
-		if err := os.Chown(dir.path, dir.uid, dir.gid); err != nil {
-			log.Error(err)
-		} else {
-			log.Info("Chown dir ", dir.path, " to mode ", dir.mode)
-		}
+	log.SetLevel(log.DebugLevel)
+	// log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	// log.SetFormatter(&log.JSONFormatter{})
+
+	for _, dir := range parseConfig() {
+		dir.chmod()
+		dir.chown()
 	}
 }
